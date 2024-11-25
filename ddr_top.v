7e4575ca0eaf016feed449a6508ea769c62938f7
Jiya <95980718+jiya11@users.noreply.github.com>
2024-11-25 16:47:26 -0500

    Update ddr_top.v
    COMPILED VERSION 1

--- ddr_top.v
+++ ddr_top.v
@@ -1,4 +1,4 @@
-// UPDATED AS OF NOV 25 12:06 am 
+// UPDATED AS OF NOV 25 4:46 pm -- COMPILED VERSION
 
 /* THIS IS THE TOP MODULE ONLY FOR THE INPUT PROCESSING MODULE */
 
@@ -6,7 +6,7 @@ module ddr_top (
     input CLOCK_50,            // 50MHz clock
     input [3:0] KEY,          // Keys (active low) - for start/pause/reset
     input [9:0] GPIO_0,       // GPIO inputs for buttons
-    output [9:0] LEDR         // LED outputs
+    output [9:0] LEDR,         // LED outputs
 
     // VGA outputs
     output reg [7:0] VGA_R,
@@ -15,8 +15,7 @@ module ddr_top (
     output reg VGA_HS, VGA_VS, VGA_BLANK_N, VGA_SYNC_N, VGA_CLK,
 
     // FROM JIYAS TOP
-    input [9:0] SW;
-    output [6:0] HEX3, HEX2, HEX1, HEX0;
+    output [6:0] HEX3, HEX2, HEX1, HEX0
 );
 
     //////////////////////////////////////////////////////////////////////////////////////
@@ -51,7 +50,7 @@ module ddr_top (
     // Instantiate the input processing module
     input_processing input_proc (
         .clock(CLOCK_50),           // 50MHz clock
-        .reset(~KEY[1]),             // Reset using SW[9]
+        .reset(~KEY[1]),             // Reset using KEY[3]
         .a_in(player_a_raw),       // Player A raw inputs
         .b_in(player_b_raw),       // Player B raw inputs
         .a_out(player_a_clean),    // Player A cleaned outputs REPLACE ALL player_a_debounced with this
@@ -106,56 +105,66 @@ module ddr_top (
     //////////////////////////////////////////////////////////////////////////////////////
 
     // SCORING
-    wire perfect_hit_a;
-	wire perfect_hit_b;
+	 wire perfect_hit_a;
+	 wire perfect_hit_b;
+	 
+	 // WINNER
+	 // Add these wire declarations
+	 wire player_a_won;
+	 wire player_b_won;
 
     // Instantiate score tracking module
-	score_tracker tracker_inst (
+	 score_tracker tracker_inst (
 		 .CLOCK_50(CLOCK_50),           // System clock
-        .reset(~KEY[1]),                 // Reset signal
+       .reset(~KEY[1]),                 // Reset signal
 		 .player_a_keys(player_a_clean),    // Player A's debounced inputs
-         .player_b_keys(player_b_clean),    // Player B's debounced inputs
+       .player_b_keys(player_b_clean),    // Player B's debounced inputs
 		 .perfect_hit_a(perfect_hit_a),         // Perfect hit signal for Player A
 		 .perfect_hit_b(perfect_hit_b),         // Perfect hit signal for Player B
+		 .game_active(game_active),
+		 .show_game_over(show_game_over),
 		 .HEX0(HEX2),                   // Player A's score - ones digit
 		 .HEX1(HEX3),                   // Player A's score - tens digit
 		 .HEX2(HEX0),                   // Player B's score - ones digit
-		 .HEX3(HEX1)                    // Player B's score - tens digit
-	);
-    
+		 .HEX3(HEX1),                    // Player B's score - tens digit
+		 .player_a_won(player_a_won),
+		 .player_b_won(player_b_won)
+	 );
 
     // PATTERN GENERATOR INPUTS
     wire [7:0] pattern_out;
     wire pattern_valid;
 
     //Instantiate pattern generator with simplified timing
-	arrow_pattern_generator pattern_gen (
+	 arrow_pattern_generator pattern_gen (
         .clock(CLOCK_50),
         .reset(~KEY[1]),
-        .game_active(1'b1),
         .pattern_out(pattern_out),
-        .pattern_valid(pattern_valid)
+        .pattern_valid(pattern_valid),
+		  .game_active(game_active)
     );
 
     //Instantiate arrow module
 	 arrow_game game_inst(
-		 .CLOCK_50(CLOCK_50),           // System clock
-         .reset(~KEY[1]),                 // Reset signal
-		 .player_a_keys(player_a_clean),    // Player A's debounced inputs
-         .player_b_keys(player_b_clean),    // Player B's debounced inputs
-		 .VGA_R(VGA_R),                 // VGA color signals
-		 .VGA_G(VGA_G),
-		 .VGA_B(VGA_B),
-		 .VGA_HS(VGA_HS),               // VGA sync signals
-		 .VGA_VS(VGA_VS),
-		 .VGA_BLANK_N(VGA_BLANK_N),
-		 .VGA_SYNC_N(VGA_SYNC_N),
-		 .VGA_CLK(VGA_CLK),
-		 .perfect_hit_a(perfect_hit_a),         // Perfect hit output for Player A
-		 .perfect_hit_b(perfect_hit_b),         // Perfect hit output for Player B
-		 .pattern_valid(pattern_valid),         // Input from pattern generator
-		 .pattern_out(pattern_out)              // Pattern data from generator
+		 .CLOCK_50(CLOCK_50),
+		 .reset(~KEY[1]),
+		 .game_active(game_active),
+		 .player_a_keys(player_a_clean),
+		 .player_b_keys(player_b_clean),
+		 .VGA_R(arrow_game_r),    // Using intermediate wires
+		 .VGA_G(arrow_game_g),
+		 .VGA_B(arrow_game_b),
+		 .VGA_HS(arrow_game_hs),
+		 .VGA_VS(arrow_game_vs),
+		 .VGA_BLANK_N(arrow_game_blank_n),
+		 .VGA_SYNC_N(arrow_game_sync_n),
+		 .VGA_CLK(arrow_game_clk),
+		 .perfect_hit_a(perfect_hit_a),
+		 .perfect_hit_b(perfect_hit_b),
+		 .pattern_valid(pattern_valid),
+		 .pattern_out(pattern_out)
 	 );
+	 // this one uses intermediate wires
     
     //////////////////////////////////////////////////////////////////////////////////////
     // FOR VGA BACKGROUNDS
@@ -168,8 +177,8 @@ module ddr_top (
     wire [7:0] countdown_vga_r, countdown_vga_g, countdown_vga_b;
     wire countdown_vga_hs, countdown_vga_vs, countdown_vga_blank_n, countdown_vga_sync_n, countdown_vga_clk;
     
-    // wire [7:0] gameplay_vga_r, gameplay_vga_g, gameplay_vga_b;
-    // wire gameplay_vga_hs, gameplay_vga_vs, gameplay_vga_blank_n, gameplay_vga_sync_n, gameplay_vga_clk;
+	 wire [7:0] arrow_game_r, arrow_game_g, arrow_game_b;
+	 wire arrow_game_hs, arrow_game_vs, arrow_game_blank_n, arrow_game_sync_n, arrow_game_clk;
     
     wire [7:0] pause_vga_r, pause_vga_g, pause_vga_b;
     wire pause_vga_hs, pause_vga_vs, pause_vga_blank_n, pause_vga_sync_n, pause_vga_clk;
@@ -316,17 +325,16 @@ module ddr_top (
             VGA_SYNC_N = pause_vga_sync_n;
             VGA_CLK = pause_vga_clk;
         end
+		  
         else if (game_active) begin
-		 // CONNECT THIS TO GAME_LOGIC_MODULES.V
-            // Show main gameplay screen
-            // VGA_R = gameplay_vga_r;
-            // VGA_G = gameplay_vga_g;
-            // VGA_B = gameplay_vga_b;
-            // VGA_HS = gameplay_vga_hs;
-            // VGA_VS = gameplay_vga_vs;
-            // VGA_BLANK_N = gameplay_vga_blank_n;
-            // VGA_SYNC_N = gameplay_vga_sync_n;
-            // VGA_CLK = gameplay_vga_clk;
+			 VGA_R = arrow_game_r;
+			 VGA_G = arrow_game_g;
+			 VGA_B = arrow_game_b;
+			 VGA_HS = arrow_game_hs;
+			 VGA_VS = arrow_game_vs;
+			 VGA_BLANK_N = arrow_game_blank_n;
+			 VGA_SYNC_N = arrow_game_sync_n;
+			 VGA_CLK = arrow_game_clk;
         end
         else if (player_a_won) begin
             // Show player A win screen
@@ -369,9 +377,7 @@ module ddr_top (
         .FPGA_I2C_SDAT(FPGA_I2C_SDAT),
         .AUD_XCK(AUD_XCK),
         .AUD_DACDAT(AUD_DACDAT),
-        .FPGA_I2C_SCLK(FPGA_I2C_SCLK),
-        .SW(SW),
-        .LEDR(LEDR)
+        .FPGA_I2C_SCLK(FPGA_I2C_SCLK)
     );
     
     // Countdown audio module
@@ -386,9 +392,7 @@ module ddr_top (
         .FPGA_I2C_SDAT(FPGA_I2C_SDAT),
         .AUD_XCK(AUD_XCK),
         .AUD_DACDAT(AUD_DACDAT),
-        .FPGA_I2C_SCLK(FPGA_I2C_SCLK),
-        .SW(SW),
-        .LEDR(LEDR)
+        .FPGA_I2C_SCLK(FPGA_I2C_SCLK)
     );
     
     // Game music module
@@ -403,9 +407,7 @@ module ddr_top (
         .FPGA_I2C_SDAT(FPGA_I2C_SDAT),
         .AUD_XCK(AUD_XCK),
         .AUD_DACDAT(AUD_DACDAT),
-        .FPGA_I2C_SCLK(FPGA_I2C_SCLK),
-        .SW(SW),
-        .LEDR(LEDR)
+        .FPGA_I2C_SCLK(FPGA_I2C_SCLK)
     );
 
 endmodule
